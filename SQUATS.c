799299fe2bb16aff33d3cff474f734d6e86e57a6
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-01-01 13:20:54 +0530

    Add files via upload

---
+++ SQUATS.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,a;
+        scanf("%d",&x);
+        a=x*15;
+        printf("%d\n",a);
+    }
+}
