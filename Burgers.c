b3b24c2a8b3c8d88ac3d522ad07911d9665f0cad
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-03 22:25:39 +0530

    Add files via upload

---
+++ Burgers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ( )
+{
+    int t ;
+    scanf ( "%d" , &t ) ;
+    while ( t-- )
+    {
+        int a , b ;
+        scanf ( "%d %d" , &a , &b ) ;
+        if ( a < b  || a == b )
+        {
+            printf ( "%d\n" , a ) ;
+            
+        }
+        else
+        {
+            printf ( "%d\n" , b ) ;
+        }
+    }
+}
