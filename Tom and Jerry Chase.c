1acf4264800c87c44653b5837078c7e74fb6b549
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-16 12:40:58 +0530

    Add files via upload

---
+++ Tom and Jerry Chase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ( )
+{
+    int t ;
+    scanf ( "%d" , &t ) ;
+    while ( t-- )
+    {
+        int a , b ;
+        scanf ( "%d%d" , &a , &b ) ;
+        if ( b > a )
+        {
+            printf ( "YES\n" ) ;
+        }
+        else
+        {
+            printf ( "NO\n" ) ; 
+        }
+    }
+}
