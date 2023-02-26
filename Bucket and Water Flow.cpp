15f0740d82cb433a535c0c6e02b9751548360c44
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-26 23:19:13 +0530

    Add files via upload

---
+++ Bucket and Water Flow.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t ;
+	while ( t-- )
+	{
+	   int a , b , c , d ;
+	cin >> a >> b >> c >> d ;
+	if ( a + c * d > b )
+	{
+	    cout << "overFlow" << endl ;
+	}
+	else if ( a + c * d == b)
+	{
+	    cout << "filled" << endl ;
+	}
+	else if ( a + c * d < b )
+	{
+	    cout << "Unfilled" << endl ;
+	}   
+	}
+	return 0;
+}
