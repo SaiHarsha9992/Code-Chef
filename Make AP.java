2381c8bc8d2df905bf98bf5556bea6119de5a573
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-16 21:14:01 +0530

    Add files via upload

---
+++ Make AP.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s  = new Scanner ( System . in ) ;
+		int t = s . nextInt () ;
+		while ( t-- > 0 ) 
+		{
+		    int c = 0 ;
+		    int a = s . nextInt () ; 
+		    int b = s . nextInt () ;
+		    for ( int  i = a  ; i <= b ; i++ )
+		    {
+		       if ( i - a == b - i )
+		        {
+		           System.out.println( i ) ;
+		           c = 1 ;
+		        }
+		    }
+		    if ( c == 0 )
+		    {
+		        System.out.println( -1 ) ;
+		    }
+		}
+		
+	}
+}
