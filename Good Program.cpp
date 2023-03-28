15d2201f29499cd0350e3b98df042f223c86169e
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-28 20:10:28 +0530

    Add files via upload

---
+++ Good Program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t ;
+	while ( t-- ) 
+	{
+	    int n ;
+	    cin >> n ;
+	    if ( n % 4 == 0 )
+	    {
+	        cout << "Good" << endl ;
+	    }
+	    else
+	    {
+	        cout << "Not Good" << endl ;
+	    }
+	}
+	return 0;
+}
