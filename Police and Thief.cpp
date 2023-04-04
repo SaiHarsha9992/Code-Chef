7fc535c7b04d3a7f594d9898bbb4d033eec8ea7d
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-04 22:07:25 +0530

    Add files via upload

---
+++ Police and Thief.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t ;
+	while ( t-- )
+	{
+	    int a , b ;
+	    cin >> a >> b ;
+	    int c = abs(a-b);
+	    cout << c << endl;
+	}
+	return 0;
+}
