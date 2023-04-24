30f2c894e3056a7c632ef6f4e143c12ec5338477
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-24 19:34:44 +0530

    Add files via upload

---
+++ Primality Test.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void chkPrime(int n){
+    bool isPrime = true;
+    if(n<2){
+        isPrime = false;
+    }
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n%i == 0){
+            isPrime = false;
+        }
+    }
+    if(isPrime){
+        cout<<"yes"<<endl;
+    } else{
+        cout<<"no"<<endl;
+    }
+}
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t ;
+	while ( t-- )
+	{
+	    int n ;
+	    cin >> n ;
+	    chkPrime(n);
+	}
+	return 0;
+}
