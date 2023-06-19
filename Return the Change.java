3db3fdc50f3114f1ac7c2c1f54205b03e77654ac
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-06-19 19:30:36 +0530

    Add files via upload

---
+++ Return the Change.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int a = 100-n;
+		    if(a%10==0) {
+		        System.out.println(a);
+		    }
+		    else 
+		    {
+		        if(a%10<=5) 
+		        {
+		            System.out.println(a-(a%10));
+		        }
+		        else 
+		        {
+		            System.out.println(a+10-(a%10));
+    		     }
+		    }
+		}
+	}
+}
