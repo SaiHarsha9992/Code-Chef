8d7a0da24a9c4dc922de62420d7573fe59f4380c
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-05 21:44:40 +0530

    Add files via upload

---
+++ Puzzle Hunt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CodeChef{
+    public static void main(String args []){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        if(n>=6&&n<=8){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
